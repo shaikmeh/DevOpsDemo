72b8247e30b679ae2be2d47a8105daf12462deff
shaikmeh <shaikmeh@students.zhaw.ch>
2024-05-08 14:45:32 +0200

    8commit

--- backend/src/test/java/ch/zhaw/iwi/devops/fizzbuzz/FizzBuzzConverterTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/fizzbuzz/FizzBuzzConverterTest.java
@@ -3,10 +3,10 @@ package ch.zhaw.iwi.devops.fizzbuzz;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class FizzBuzzConverterTest {
+       class FizzBuzzConverterTest {
 
     @Test
-    public void fizzBuzzConverter1() {
+         void fizzBuzzConverter1() {
 
         FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
         Assertions.assertEquals("1", fizzBuzz.convert(1));
@@ -14,7 +14,7 @@ public class FizzBuzzConverterTest {
     }
 
     @Test
-    public void fizzBuzzConvertor2() {
+         void fizzBuzzConvertor2() {
 
         FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
         Assertions.assertEquals("2", fizzBuzz.convert(2));
@@ -22,7 +22,7 @@ public class FizzBuzzConverterTest {
     }
 
     @Test
-    public void fizzBuzzConvertor3() {
+        void fizzBuzzConvertor3() {
 
         FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
 
@@ -30,7 +30,7 @@ public class FizzBuzzConverterTest {
     }
     
     @Test
-    public void fizzBuzzConvertorMultiplesOfThree() {
+         void fizzBuzzConvertorMultiplesOfThree() {
 
         FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
 
@@ -38,7 +38,7 @@ public class FizzBuzzConverterTest {
     }
 
     @Test
-    void fizzBuzzConvertorMultiplesOfSeven() {
+        public void fizzBuzzConvertorMultiplesOfSeven() {
 
         FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
 
